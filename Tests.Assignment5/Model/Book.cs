89fa55c229f5ea920110b621ce7c3b3833b4fd75
GusRodrigues <omykronbr@gmail.com>
2019-11-20 23:56:51 -0500

    Immutability TestCase Equality TestCase

---
+++ Tests.Assignment5/Model/Book.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Assignment5.Model
+{
+    /// <summary>
+    /// <para>Book is immutable.</para>
+    /// <para>Book contains information about:</para>
+    /// <para>Title</para>
+    /// <para>List of Authors</para>
+    /// <para>Copyright Year</para>
+    /// <para>Number of Pages</para>
+    /// </summary>
+    public class Book
+    {
+        private readonly string _title;
+        private readonly List<string> _authors;
+        private readonly int _copyrightYear;
+        private readonly int _numberOfPages;
+        /* Rep Invariant:
+         *      Title != null + white spaced.
+         *      List of Authors != non null + size >= 1
+         *      1900 < copyrightYear <= 2018
+         *      numberOfPages >= 1
+         *              
+         * AF(title,author,copyrightYear,numberOfPages) = title * author + copyrightYear + numberOfPages
+         */
+
+        
+        /// <summary>
+        /// Creates a new book
+        /// </summary>
+        /// <param name="title">The title of the book</param>
+        /// <param name="author">The author(s) of the book</param>
+        /// <param name="copyrightYear">the year of release</param>
+        /// <param name="numberOfPages">number of pages</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public Book(string title, List<string> authors, int copyrightYear, int numberOfPages)
+        {
+            _title = title;
+            _authors = new List<string>(authors);
+            _copyrightYear = copyrightYear;
+            _numberOfPages = numberOfPages;
+            _CheckRep();
+        }
+        // simple observers of this class.
+
+        public string GetTitle() => _title;
+        public List<string> GetAuthors() => new List<string>(_authors);
+        public int GetCopyrightYear() => _copyrightYear;
+        public int GetNumberOfPages() => _numberOfPages;
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Book book))
+            {
+              return false;
+            }
+            Book other = (Book) obj;
+            return _title == book._title &&
+                   _AuthorsEquality(other.GetAuthors()) &&
+                   _copyrightYear == book._copyrightYear &&
+                   _numberOfPages == book._numberOfPages;
+        }
+        
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_title, _authors, _copyrightYear, _numberOfPages);
+        }
+
+        public override string ToString()
+        {
+            return $"[{_title} by {_authors} - p. {_numberOfPages}. Copyright: {_copyrightYear}]";
+        }
+
+        private bool _AuthorsEquality(List<string> toCompare)
+        {
+            HashSet<string> actual = new HashSet<string>(_authors);
+            HashSet<string> other = new HashSet<string>(toCompare);
+            return (actual.SetEquals(other)) ? true : false;
+        }
+
+        /// <summary>
+        /// Checks class invariance and AF.
+        /// </summary>
+        private void _CheckRep()
+        {
+            // title invariance checker: Title != null + white spaced.
+            _ = (String.IsNullOrWhiteSpace(_title)) ? throw new ArgumentNullException("title", "The title cannot be empty") : "";
+            // author invariance checker: List of Authors != non null + size >= 1
+            _ = _authors ?? throw new ArgumentNullException("author", "author cannot be null");
+            _ = (_authors.Count < 1 || String.IsNullOrWhiteSpace(_authors[0])) ? throw new ArgumentNullException("At least one Author must be not Whitespaced") : "";
+            // 1900 < copyrightYear <= 2018
+            _ = (_copyrightYear > 2019 || _copyrightYear < 1900 ) ? throw new ArgumentOutOfRangeException("Copyright Year", "Invalid Copyright Year") : "";
+            // numberOfPages >= 1
+            _ = (_numberOfPages < 1) ? throw new ArgumentOutOfRangeException("Number of Pages","A book cannot have less than one page.") : "";
+        }
+    }
+}
---
+++ Tests.Assignment5/Model/BookTest.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Assignment5.Model
+{
+    class BookTest
+    {
+        public Book book;
+        [SetUp]
+        public void init() 
+        {
+            string title = "1984";
+            List<string> authors = new List<string>();
+            authors.Add("George Orwell");
+            int copyrightRelease = 1949;
+            int numberOfPages = 328;
+            book = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: numberOfPages);
+        }
+
+        [Test]
+        public void Test_BookIsImmutable()
+        {
+            // Assemble
+            string title = "1984";
+            List<string> authors = new List<string>();
+            authors.Add("George Orwell");
+            int copyrightRelease = 1949;
+            int numberOfPages = 328;
+            book = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: numberOfPages);
+            // Act by changing everything
+            // change title
+            title = "Brave new World";
+            // add author to list
+            authors.Add("Bob");
+            // change release date
+            copyrightRelease = 1984;
+            // number of pages
+            numberOfPages = 200;
+            Book secondBook = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: numberOfPages);
+            // Assert
+            Assert.That(book.GetTitle(), Is.Not.EqualTo(title));
+            Assert.That(book.GetAuthors(), Is.Not.EqualTo(authors));
+            Assert.That(book.GetCopyrightYear(), Is.Not.EqualTo(copyrightRelease));
+            Assert.That(book.GetNumberOfPages(), Is.Not.EqualTo(numberOfPages));
+            Assert.That(book, Is.Not.EqualTo(secondBook));
+        }
+
+        [Test]
+        public void Test_ForEquality()
+        {
+            // Assemble
+            Book secondBook;
+            Book thirdBook;
+            Book invalidBook;
+            bool reflexive;
+            bool symmetric;
+            bool transitive;
+            bool invalid;
+            string title = "1984";
+            List<string> authors = new List<string>();
+            authors.Add("George Orwell");
+            int copyrightRelease = 1949;
+            int numberOfPages = 328;
+            secondBook = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: numberOfPages);
+            thirdBook = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: numberOfPages);
+            invalidBook = new Book(title: title, authors: authors, copyrightYear: copyrightRelease, numberOfPages: 1);
+            // Act
+            reflexive = book.Equals(book);
+            symmetric = (book.Equals(secondBook) && secondBook.Equals(book));
+            transitive = (book.Equals(secondBook) && secondBook.Equals(thirdBook) && book.Equals(thirdBook));
+            invalid = book.Equals(invalidBook);
+            // Assert
+            // reflexive
+            Assert.That(reflexive, Is.True);
+            Assert.That(symmetric, Is.True);
+            Assert.That(transitive, Is.True);
+            Assert.That(invalid, Is.False);
+        }
+
+    }
+}
